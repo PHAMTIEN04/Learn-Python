5d4c105b0aad255173eae08d9b1ec5d80ffcefff
PHAMTIEN04 <phamphuoctien177@gmail.com>
2023-04-23 23:58:25 +0700

    update

---
+++ Bài Tập Kỹ Thuật Lập Trình/Bangcuuchuong.py
@@ -0,0 +1,30 @@
+i = 1
+j = 1
+while i < 6:
+    
+    if i == 1 and j == 1 :
+        print("Bảng cửu chương 1 :")
+    elif i == 2 and j == 1 :
+        print("Bảng cửu chương 2 :")
+    elif i == 3 and j == 1 :
+        print("Bảng cửu chương 3 :")
+    elif i == 4 and j == 1 :
+        print("Bảng cửu chương 4 :")
+    elif i == 5 and j == 1 :
+        print("Bảng cửu chương 5 :")
+    elif i == 6 and j == 1 :
+        print("Bảng cửu chương 6 :")
+    elif i == 7 and j == 1 :
+        print("Bảng cửu chương 7 :")
+    elif i == 8 and j == 1 :
+        print("Bảng cửu chương 8 :")
+    elif i == 9 and j == 1 :
+        print("Bảng cửu chương 9 :")
+    elif i == 10 and j == 1 :
+        print("Bảng cửu chương 10 :")
+    print(i,"x",j ,"=",i*j,sep=" ")
+    if j == 10 :
+        i = i+1
+        j = 1
+    else :
+        j = j+1
