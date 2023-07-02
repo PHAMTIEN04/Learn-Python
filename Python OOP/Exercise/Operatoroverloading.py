1231e3687a9efb805936b5d401918ef12c51b378
PHAMTIEN04 <phamphuoctien177@gmail.com>
2023-07-03 01:58:18 +0700

    update

--- Python OOP/Exercise/Operatoroverloading.py
+++ Python OOP/Exercise/Operatoroverloading.py
@@ -1,21 +1,73 @@
+def gcd(a, b):
+    while b:
+        a, b = b, a % b
+    return a
+
+
+def rutgon(a, b):
+    gtc = gcd(a, b)
+    ts_rg = a // gtc
+    ms_rg = b // gtc
+    return "{}/{}".format(ts_rg, ms_rg) if gtc != 1 else ""
+
 
 class Operator_overloading:
-    def __init__(self,name,pay) :
-        self.name = name
-        self.pay = pay
+    def __init__(self, tu, mau):
+        self.tu = tu
+        self.mau = mau
     
-    def __add__(self,other):
-        return self.pay + other.pay
-    def __sub__(self,other):
-        return self.pay - other.pay
-    def __mul__(self,other):
-        return self.pay * other.pay
-    def __div__(self,other):
-        return self.pay / other.pay
-o1 = Operator_overloading("Trump",1000)
-o2 = Operator_overloading("Biden",2000)
+    def __add__(self, other):
+        ts = ((self.tu * other.mau) + (other.tu * self.mau))
+        ms = (self.mau * other.mau)
+        return Operator_overloading(ts, ms)
+    
+    def __sub__(self, other):
+        ts = ((self.tu * other.mau) - (other.tu * self.mau))
+        ms = (self.mau * other.mau)
+        return Operator_overloading(ts, ms)
+    
+    def __mul__(self, other):
+        ts = self.tu * other.tu
+        ms = other.mau * self.mau
+        return Operator_overloading(ts, ms)
+    
+    def __truediv__(self, other):
+        ts = self.tu * other.mau
+        ms = self.mau * other.tu
+        return Operator_overloading(ts, ms)
 
-print(o1 + o2)
-print(o1 - o2)
-print(o1 * o2)
-print(o1.__div__(o2))
+
+o1 = Operator_overloading(1, 2)
+o2 = Operator_overloading(3, 4)
+
+result_add = o1 + o2
+result_add_rutgon = rutgon(result_add.tu, result_add.mau)
+print(o1.tu, "/", o1.mau, " + ", o2.tu, "/", o2.mau, " = ", result_add.tu, "/", result_add.mau, sep="",end="")
+if result_add_rutgon:
+    print(" =", result_add_rutgon)
+else:
+    print()
+
+result_sub = o1 - o2
+result_sub_rutgon = rutgon(result_sub.tu, result_sub.mau)
+print(o1.tu, "/", o1.mau, " - ", o2.tu, "/", o2.mau, " = ", result_sub.tu, "/", result_sub.mau, sep="",end="")
+if result_sub_rutgon:
+    print(" =", result_sub_rutgon)
+else:
+    print()
+
+result_mul = o1 * o2
+result_mul_rutgon = rutgon(result_mul.tu, result_mul.mau)
+print(o1.tu, "/", o1.mau, " * ", o2.tu, "/", o2.mau, " = ", result_mul.tu, "/", result_mul.mau,sep="", end="")
+if result_mul_rutgon:
+    print(" =", result_mul_rutgon)
+else:
+    print()
+
+result_div = o1 / o2
+result_div_rutgon = rutgon(result_div.tu, result_div.mau)
+print(o1.tu, "/", o1.mau, " / ", o2.tu, "/", o2.mau, " = ", result_div.tu, "/", result_div.mau,sep="", end="")
+if result_div_rutgon:
+    print(" =", result_div_rutgon)
+else:
+    print()
--- Tool Python/Project Airdrop Onus/Autoairdrop.py
+++ Tool Python/Project Airdrop Onus/Autoairdrop.py
@@ -1,4 +1,3 @@
-# Incomplete
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
@@ -11,9 +10,9 @@ import string
 import random
 from time import sleep
 import os
-
-user_agent = UserAgent()
-random_user_agent = user_agent.random
+import cv2
+import pytesseract
+from PIL import Image
 
 # Set the project directory
 project_directory = os.path.abspath("D:/Learn Python/Tool Python/Project Airdrop Onus")
@@ -21,12 +20,13 @@ os.chdir(project_directory)
 
 # Set Chrome options
 chrome_options = webdriver.ChromeOptions()
-# chrome_options.add_argument('--incognito')
+chrome_options.add_argument('--incognito')
 prefs = {
     "profile.managed_default_content_settings.images": 2
 }
-# chrome_options.add_experimental_option('prefs', prefs)
-chrome_options.add_argument(f'user-agent={random_user_agent}')
+chrome_options.add_experimental_option('prefs', prefs)
+chrome_options.add_argument(f'user-agent={UserAgent().random}')
+
 # Set the driver path
 driver_path = os.path.join(project_directory, "chromedriver")
 service = Service(executable_path=driver_path)
@@ -35,10 +35,6 @@ service = Service(executable_path=driver_path)
 driver = webdriver.Chrome(service=service, options=chrome_options)
 driver1 = webdriver.Chrome(service=service, options=chrome_options)
 driver2 = webdriver.Chrome(service=service, options=chrome_options)
-
-# Get the random user agent
-user_agent = UserAgent()
-random_user_agent = user_agent.random
 
 # Get the email address from 10minutemail.net
 driver2.get("https://10minutemail.net/")
@@ -50,12 +46,10 @@ print(email_address)
 driver1.get("https://signup.goonus.io/")
 account_reg = driver1.find_element(By.NAME, 'account_reg')
 
-
 # Generate a random password
 password_reg = driver1.find_element(By.NAME, 'password_reg')
 password = ''.join(random.choices(string.ascii_letters + string.digits, k=random.choice([9, 10, 11, 12, 13, 14, 15, 16])))
 print(password)
-
 
 # Generate a random full name
 list_fullname = ["Nguyễn", "Trần", "Lê", "Phạm", "Hoàng", "Huỳnh", "Phan", "Vũ", "Đặng", "Bùi", "Đỗ", "Hồ", "Ngô", "Dương", "Lý", "Quách", "Trương", "Võ", "Đinh", "Lương", "Lỗ", "Chử", "Mai", "Thái", "Tô", "Đoàn", "Hứa", "Thanh", "Đào", "Đỗ", "Thủy", "Lưu", "Tiền", "Kiều", "Trịnh", "Chu", "Lữ", "Khương", "Tăng", "Khoa", "Tiêu", "Ninh", "Phí", "Uông", "Châu", "Khâu", "Vi", "Lã", "Giang", "Tôn", "Kỷ", "Tăng", "Viên", "Khâu", "Viên", "Tiếp", "Sơn", "Dục", "Tôn", "Đoàn", "Tề", "Tử", "Vương", "Thường", "Phong", "Thiên", "Hoài", "Tiến", "Hiếu", "Mạnh", "Ngọc", "Huy", "Hiệp", "Tùng", "Hùng", "Bảo", "An", "Quân", "Bình", "Hải", "Xuân", "Minh", "Dũng", "Tân", "Phúc", "Quang", "Trung", "Hải", "Hà", "Việt", "Công", "Quốc", "Thắng", "Thành", "Lực", "Quyết", "Thịnh", "Vân", "Hào", "Anh"]
@@ -63,34 +57,55 @@ name = ' '.join(random.choices(list_fullname, k=2))
 print(name)
 fullname_reg = driver1.find_element(By.NAME, 'fullname_reg')
 
-
+# Checkbox handling options
 checkbox_element = driver1.find_element(By.XPATH, "//input[@type='checkbox']")
+checkbox_options = [
+    (account_reg, password_reg, fullname_reg),
+    (account_reg, fullname_reg, password_reg),
+    (password_reg, account_reg, fullname_reg),
+    (fullname_reg, account_reg, password_reg)
+]
+random.shuffle(checkbox_options)
 
-o = ["1","2","3","4"]
-o_r = random.choice(o)
-print(o_r)
-if o_r == "1":
+for account_element, password_element, fullname_element in checkbox_options:
     checkbox_element.click()
-    account_reg.send_keys(email_address)
-    password_reg.send_keys(password)
-    fullname_reg.send_keys(name)
-elif o_r == "2":
-    account_reg.send_keys(email_address)
-    checkbox_element.click()
-    password_reg.send_keys(password)
-    fullname_reg.send_keys(name)
-elif o_r == "3":
-    password_reg.send_keys(password)
-    checkbox_element.click()
-    account_reg.send_keys(email_address)
-    fullname_reg.send_keys(name)
-elif o_r == "4":
-    fullname_reg.send_keys(name)
-    checkbox_element.click()
-    account_reg.send_keys(email_address)
-    password_reg.send_keys(password)
+    account_element.send_keys(email_address)
+    password_element.send_keys(password)
+    fullname_element.send_keys(name)
 
-# checkbox_element.send_keys(Keys.ENTER)
+    # Check if CAPTCHA is present
+# Check if CAPTCHA is present
+captcha_element = driver1.find_element(By.CSS_SELECTOR, "img.captcha")
+if captcha_element:
+    # Process the CAPTCHA image
+    captcha_image_url = captcha_element.get_attribute("src")
+    driver1.save_screenshot("screenshot.png")
+    location = captcha_element.location
+    size = captcha_element.size
+    left = int(location['x'])
+    top = int(location['y'])
+    right = int(location['x'] + size['width'])
+    bottom = int(location['y'] + size['height'])
+    image = cv2.imread("screenshot.png")
+    captcha_image = image[top:bottom, left:right]
+    captcha_image_path = os.path.join(project_directory, "captcha_image.png")
+    cv2.imwrite(captcha_image_path, captcha_image)
+
+    # Process the CAPTCHA image using OCR
+    captcha_text = pytesseract.image_to_string(Image.open(captcha_image_path))
+
+    if captcha_text:
+        captcha_input = driver1.find_element(By.NAME, 'captcha')
+        captcha_input.send_keys(captcha_text)
+        
+
+        
+    checkbox_element.click()
+    account_element.clear()
+    password_element.clear()
+    fullname_element.clear()
+
+# Submit the form
 reg = "document.querySelector('.text').click()"
 driver1.execute_script(reg)
 
@@ -98,7 +113,6 @@ driver1.execute_script(reg)
 sleep(120)
 reset_mail = "document.querySelector('.fa.fa-refresh.fa-fw.fa-lg ').click()"
 driver2.execute_script(reset_mail)
-
 driver2.refresh()
 
 click_mail = "document.querySelector('.row-link').click()"
@@ -112,8 +126,6 @@ driver.get("https://earn.goonus.io/projects/ONUS-Airdrop-USDT?eid=sHQvR-ftZqUal5
 
 element = driver.find_element(By.XPATH, "//button[contains(text(), 'Tham gia Airdrop')]")
 element.click()
-
-sleep(1000)
 
 # Quit the drivers
 driver.quit()
