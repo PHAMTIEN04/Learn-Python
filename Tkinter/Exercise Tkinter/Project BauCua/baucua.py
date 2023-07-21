484ba5f94a7e179b6fb0d68b10dd1438e571f711
PHAMTIEN04 <phamphuoctien177@gmail.com>
2023-07-21 18:57:12 +0700

    update

--- Tkinter/Exercise Tkinter/Project BauCua/baucua.py
+++ Tkinter/Exercise Tkinter/Project BauCua/baucua.py
@@ -7,22 +7,27 @@ from PIL import Image,ImageTk
 win = Tk()
 def resize_img(path,width,height):
     img = Image.open(path)
-    img = img.resize((width,height), Image.ANTIALIAS)
+    img = img.resize((width,height), Image.LANCZOS)
     return ImageTk.PhotoImage(img)
 width = 60
 height = 60
-img_bau = "D:/Learn Python/Tkinter/Exercise Tkinter/Project BauCua/bau.jpg"
-img_cua = "D:/Learn Python/Tkinter/Exercise Tkinter/Project BauCua/cua.jpg"
-img_tom= "D:/Learn Python/Tkinter/Exercise Tkinter/Project BauCua/tom.jpg"
-img_ca = "D:/Learn Python/Tkinter/Exercise Tkinter/Project BauCua/ca.jpg"
-img_ga = "D:/Learn Python/Tkinter/Exercise Tkinter/Project BauCua/ga.jpg"
-img_nai = "D:/Learn Python/Tkinter/Exercise Tkinter/Project BauCua/nai.jpg"
+img_bau = "./bau.jpg"
+img_cua = "./cua.jpg"
+img_tom= "./tom.jpg"
+img_ca = "./ca.jpg"
+img_ga = "./ga.jpg"
+img_nai = "./nai.jpg"
 
 width_m = 70
 height_m = 20
 
-img_100k = "D:/Learn Python/Tkinter/Exercise Tkinter/Project BauCua/100k.jpg"
+img_50k = "./50k.jpg"
+img_100k = "./100k.jpg"
+img_500k = "./500k.jpg"
+
+rs_50k = resize_img(path=img_50k,width=width_m,height=height_m)
 rs_100k = resize_img(path=img_100k,width=width_m,height=height_m)
+rs_500k = resize_img(path=img_500k,width=width_m,height=height_m)
 
 
 win.geometry("500x500")
@@ -45,61 +50,161 @@ lb_3 = Label(win,text="1",width=width,height=height,bg="white",image=rs_t)
 lb_3.grid(column=3,row=0,padx=20,pady=10)
 check = []
 
+animal_bet = []
+global check_money
+
 def click_nai():
     global check
     check.append("nai")
     global check_money
-    global money_nai
-    money_nai = Label(win,text="",image=rs_100k)
-    if check_money == 100 :
-        money_nai.grid(column=1,row=2)
+    
+    global money_nai50
+    global money_nai100
+    global money_nai500
+    money_nai50 = Label(win,text="",image=rs_50k)
+    money_nai100 = Label(win,text="",image=rs_100k)
+    money_nai500 = Label(win,text="",image=rs_500k)
+    if check_money == 50 :
+        money_nai50.grid(column=1,row=2)
+        animal_bet.append(money_nai50)
         win.update()
+    if check_money == 100 :
+        money_nai100.grid(column=1,row=2)
+        animal_bet.append(money_nai100)
+        win.update()
+    if check_money == 500 :
+        money_nai500.grid(column=1,row=2)
+        animal_bet.append(money_nai500)
+        win.update()
+    check_money = 0
         
 def click_bau():
     global check
     check.append("bau")
-    global money_bau
-    money_bau = Label(win,text="",image=rs_100k)
-    if check_money == 100 :
-        money_bau.grid(column=2,row=2)
+    global check_money
+    global money_bau50
+    global money_bau100
+    global money_bau500
+    money_bau50 = Label(win,text="",image=rs_50k)
+    money_bau100 = Label(win,text="",image=rs_100k)
+    money_bau500 = Label(win,text="",image=rs_500k)
+    if check_money == 50 :
+        money_bau50.grid(column=2,row=2)
+        animal_bet.append(money_bau50)
         win.update()
+    if check_money == 100 :
+        money_bau100.grid(column=2,row=2)
+        animal_bet.append(money_bau100)
+        win.update()
+    if check_money == 500 :
+        money_bau500.grid(column=2,row=2)
+        animal_bet.append(money_bau500)
+        win.update()
+    check_money = 0
 def click_ga():
     global check
     check.append("ga")
-    global money_ga
-    money_ga = Label(win,text="",image=rs_100k)
-    if check_money == 100 :
-        money_ga.grid(column=3,row=2)
+    global check_money
+    global money_ga50
+    global money_ga100
+    global money_ga500
+    money_ga50 = Label(win,text="",image=rs_50k)
+    money_ga100 = Label(win,text="",image=rs_100k)
+    money_ga500 = Label(win,text="",image=rs_500k)
+    if check_money == 50 :
+        money_ga50.grid(column=3,row=2)
+        animal_bet.append(money_ga50)
         win.update()
+    if check_money == 100 :
+        money_ga100.grid(column=3,row=2)
+        animal_bet.append(money_ga100)
+        win.update()
+    if check_money == 500 :
+        money_ga500.grid(column=3,row=2)
+        animal_bet.append(money_ga500)
+        win.update()
+    check_money = 0
 def click_ca():
     global check
     check.append("ca")
-    global money_ca
-    money_ca = Label(win,text="",image=rs_100k)
-    if check_money == 100 :
-        money_ca.grid(column=1,row=3)
+    global check_money
+    global money_ca50
+    global money_ca100
+    global money_ca500
+    money_ca50 = Label(win,text="",image=rs_50k)
+    money_ca100 = Label(win,text="",image=rs_100k)
+    money_ca500 = Label(win,text="",image=rs_500k)
+    if check_money == 50 :
+        money_ca50.grid(column=1,row=3)
+        animal_bet.append(money_ca50)
         win.update()
+    if check_money == 100 :
+        money_ca100.grid(column=1,row=3)
+        animal_bet.append(money_ca100)
+        win.update()
+    if check_money == 500 :
+        money_ca500.grid(column=1,row=3)
+        animal_bet.append(money_ca500)
+        win.update()
+    check_money = 0
 def click_cua():
     global check
     check.append("cua")
-    global money_cua
-    money_cua = Label(win,text="",image=rs_100k)
-    if check_money == 100 :
-        money_cua.grid(column=2,row=3)
+    global check_money
+    global money_cua50
+    global money_cua100
+    global money_cua500
+    money_cua50 = Label(win,text="",image=rs_50k)
+    money_cua100 = Label(win,text="",image=rs_100k)
+    money_cua500 = Label(win,text="",image=rs_500k)
+    if check_money == 50 :
+        money_cua50.grid(column=2,row=3)
+        animal_bet.append(money_cua50)
         win.update()
+    if check_money == 100 :
+        money_cua100.grid(column=2,row=3)
+        animal_bet.append(money_cua100)
+        win.update()
+    if check_money == 500 :
+        money_cua500.grid(column=2,row=3)
+        animal_bet.append(money_cua500)
+        win.update()
+    check_money = 0
 def click_tom():
     global check
     check.append("tom")
-    global money_tom
-    money_tom = Label(win,text="",image=rs_100k)
-    if check_money == 100 :
-        money_tom.grid(column=3,row=3)
+    global check_money
+    global money_tom50
+    global money_tom100
+    global money_tom500
+    money_tom50 = Label(win,text="",image=rs_50k)
+    money_tom100 = Label(win,text="",image=rs_100k)
+    money_tom500 = Label(win,text="",image=rs_500k)
+    if check_money == 50 :
+        money_tom50.grid(column=3,row=3)
+        animal_bet.append(money_tom50)
         win.update()
+    if check_money == 100 :
+        money_tom100.grid(column=3,row=3)
+        animal_bet.append(money_tom100)
+        win.update()
+    if check_money == 500 :
+        money_tom500.grid(column=3,row=3)
+        animal_bet.append(money_tom500)
+        win.update()
+    check_money = 0
 
+def click_50k():
+    global check_money 
+    check_money = 50
+    
 def click_100k():
     global check_money 
     check_money = 100
-    
+
+def click_500k():
+    global check_money 
+    check_money = 500
 bt_nai = Button(win,text="1",width=width+70,height=height+50,bg="white",image=rs_n,command=click_nai)
 bt_nai.grid(column=1, row= 2,padx=20,pady=20)
 bt_bau = Button(win,text="1",width=width+70,height=height+50,bg="white",image=rs_b,command=click_bau)
@@ -118,7 +223,7 @@ bt_tom.grid(column=3, row= 3,padx=20,pady=20)
     
 
     
-    
+list_check = []    
 
 def update_rand(label1,label2,label3):
     global check
@@ -151,54 +256,71 @@ def update_rand(label1,label2,label3):
         a_b = resize_b
         a_b = "bau"
         if a_b in check:
-            print("Thắng Bầu")
+            list_check.append(money_bau50)
+            list_check.append(money_bau100)
+            list_check.append(money_bau500)
+            print("Thang Bau")
     if r_a == resize_c or r_b == resize_c or r_c == resize_c:
         a_c = resize_c
         a_c = "cua"
         if a_c in check:
-            print("Thắng Cua")
+            list_check.append(money_cua50)
+            list_check.append(money_cua100)
+            list_check.append(money_cua500)
+            print("Thang Cua")
     if r_a == resize_t or r_b == resize_t or r_c == resize_t:
         a_t = resize_t
         a_t = "tom"
         if a_t in check:
-            print("Thắng Tôm")
+            list_check.append(money_tom50)
+            list_check.append(money_tom100)
+            list_check.append(money_tom500)
+            print("Thang Tom")
     if r_a == resize_ca or r_b == resize_ca or r_c == resize_ca:
         a_ca = resize_ca
         a_ca = "ca"
         if a_ca in check:
-            print("Thắng Cá")
+            list_check.append(money_ca50)
+            list_check.append(money_ca100)
+            list_check.append(money_ca500)
+            print("Thang Ca")
     if r_a == resize_g or r_b == resize_g or r_c == resize_g:
         a_g = resize_g
         a_g = "ga"
         if a_g in check:
-            print("Thắng Gà")
+            list_check.append(money_ga50)
+            list_check.append(money_ga100)
+            list_check.append(money_ga500)
+            print("Thang Ga")
     if r_a == resize_n or r_b == resize_n or r_c == resize_n:
         a_n = resize_n
         a_n = "nai"
         if a_n in check:
-            print("Thắng Nai")
+            list_check.append(money_nai50)
+            list_check.append(money_nai100)
+            list_check.append(money_nai500)
+            print("Thang Nai")
     check = []
     
     
     
 def run():
-    global money_nai
-    global money_bau
-    global money_ga
-    global money_ca
-    global money_cua
-    global money_tom
-    update_rand(lb_1,lb_2,lb_3)
-    money_nai.grid_remove()
-    money_bau.grid_remove()
-    money_ga.grid_remove()
-    money_ca.grid_remove()
-    money_cua.grid_remove()
-    money_tom.grid_remove()
-    win.update()
-    
-    
 
+    global list_n
+    update_rand(lb_1,lb_2,lb_3)
+    for widget in animal_bet:
+        if widget not in list_check :
+            widget.grid_remove()
+
+    win.update()
+
+    
+def tinhtien():
+    for widget in animal_bet:
+        widget.grid_remove()
+    list_check.clear()
+    animal_bet.clear()
+    win.update()
 
 bt = Button(win,text="Run",command=run)
 bt.grid(column=2,row=1)
@@ -207,8 +329,19 @@ lb_money = Label(win,text="Chọn số tiền đặt cược :",bg="red",font=("
 lb_money.grid(column=1,row=4,columnspan=1)
 
 
-money_100k = Button(win,text="1",image=rs_100k,command=click_100k)
-money_100k.grid(column=1,row=5,pady=10)
+money_50k = Button(win,text="1",image=rs_50k,command=click_50k)
+money_50k.grid(column=1,row=5,pady=10)
 
+money_100k = Button(win,text="1",image=rs_100k,command=click_100k)
+money_100k.grid(column=2,row=5,pady=10)
+
+money_500k = Button(win,text="1",image=rs_500k,command=click_500k)
+money_500k.grid(column=3,row=5,pady=10)
+
+win1 = Toplevel()
+win1["bg"] = "red"
+win1.geometry("250x250")
+bt_tt = Button(win1,text="Tính Tiền :",font=("Arial", 12, "bold"),command=tinhtien)
+bt_tt.grid(column=0,row=0,padx=10,pady=10)
 
 win.mainloop()
