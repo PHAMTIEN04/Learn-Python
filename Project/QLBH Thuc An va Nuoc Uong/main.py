a97e082de07af2cb970c02975c8f6aa2fc1e9e38
PHAMTIEN04 <phamphuoctien177@gmail.com>
2023-08-14 01:19:58 +0700

    update

--- Project/QLBH Thuc An va Nuoc Uong/main.py
+++ Project/QLBH Thuc An va Nuoc Uong/main.py
@@ -1,29 +1,72 @@
 from tkinter import *
-from PIL import Image, ImageTk
-
-def create_circle_image(radius, color):
-    width = radius * 2
-    height = radius * 2
-    image = Image.new("RGBA", (width, height))
-    for y in range(height):
-        for x in range(width):
-            dx = abs(x - radius)
-            dy = abs(y - radius)
-            distance = (dx ** 2 + dy ** 2) ** 0.5
-            if distance <= radius:
-                image.putpixel((x, y), color)
-    return ImageTk.PhotoImage(image)
+import sqlite3
+from PIL import Image,ImageTk
 
 win = Tk()
-win.title("Hình Tròn trong Tkinter")
-win.geometry("300x300")
+win.title("Quản Lý Bán Hàng")
+win.geometry("1200x700+200+20")
+win.resizable(False, False)  # Không cho phép thay đổi kích thước theo chiều ngang và dọc
 
-circle_radius = 100
-circle_color = (0, 0, 0, 255)  # Màu đen
 
-circle_image = create_circle_image(circle_radius, circle_color)
+def home():
+    pass
 
-label = Label(win, image=circle_image, borderwidth=0, highlightthickness=0)
-label.pack()
+def menu():
+    bt_home1 = Button(win, text="Thức Ăn", fg="white", bg="gray", border=0)
+    bt_home1.place(relx=0.01,rely=0.28)
+    bt_home2 = Button(win, text="Đồ Uống", fg="white", bg="gray", border=0)
+    bt_home2.place(relx=0.01,rely=0.33)
+
+labe_nen = Label(win, text="", bg="gray", width=30, height=60)
+labe_nen.grid(column=0, rowspan=13)
+
+Button_logo = Button(win, text="Logo", fg="white", bg="gray", border=0)
+Button_logo.grid(column=0, row=0, rowspan=1, sticky="NW")
+
+Button_home = Button(win, text="Trang Chủ", fg="white", bg="gray", border=0, command=home)
+Button_home.grid(column=0, row=1, rowspan=1, sticky="NW")
+
+Button_menu = Button(win, text="Sản Phẩm", fg="white", bg="gray", border=0, command=menu)
+Button_menu.grid(column=0, row=2, rowspan=1, sticky="NW")
+
+# class ThucAn():
+#     def
+def resize_img(path,width,height):
+    img = Image.open(path)
+    rez_img = img.resize((width,height), Image.LANCZOS)
+    return ImageTk.PhotoImage(rez_img)
+def get_value_dtb(stt):
+    conn = sqlite3.connect("QLBHTANU.db")
+    c = conn.cursor()
+    c.execute(f"SELECT * FROM thucan WHERE stt = {stt}")
+    res = c.fetchall()
+    vl = res[0][1]
+    conn.commit()
+    conn.close()
+    return str(vl)
+bu_img = resize_img("D:/Learn Python/Project/QLBH Thuc An va Nuoc Uong/banhuot.jpg",200,100)
+bu_lb_img = Label(win,image=bu_img,borderwidth=0,highlightthickness=0)
+bu_lb_img.place(relx=0.23,rely=0.1)
+bu_lb_text = Label(win,text=get_value_dtb(1))
+bu_lb_text.place(rely=0.26,relx=0.28)
 
 win.mainloop()
+# import sqlite3
+
+# conn = sqlite3.connect("QLBHTANU.db")
+# c = conn.cursor()
+# c.execute("""CREATE TABLE thucan(
+#         stt integer,
+#         ten text,
+#         gia integer    
+#     )""")
+# c.execute("INSERT INTO thucan VALUES(:stt,:ten,:gia)",{
+#         'stt' : 1,
+#         'ten' : "Bánh Ướt",
+#         "gia" : 20000
+# })
+# c.execute("SELECT * FROM thucan")
+# res = c.fetchall()
+# print(res)
+# conn.commit()
+# conn.close()
