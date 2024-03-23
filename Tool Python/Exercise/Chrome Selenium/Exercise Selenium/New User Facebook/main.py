678c9d02bca900ea3314421da3b4f46b3955215b
PHAMTIEN04 <phamphuoctien177@gmail.com>
2024-03-24 00:25:39 +0700

    update

--- Tool Python/Exercise/Chrome Selenium/Exercise Selenium/New User Facebook/main.py
+++ Tool Python/Exercise/Chrome Selenium/Exercise Selenium/New User Facebook/main.py
@@ -1,34 +1,69 @@
 from selenium.webdriver import Chrome
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
 from time import sleep
 import re
+import os
+from pynput.keyboard import Key,Controller
 # D:\Learn Python\Tool Python\Exercise\Chrome Selenium\Exercise Selenium\Interact Facebook\Facebook account\Account 0
-
+import pyautogui
+import pygetwindow as gw
 options = Options()
-options.add_argument(f"--user-data-dir=D:\Learn Python\Tool Python\Exercise\Chrome Selenium\Exercise Selenium\Interact Facebook\Facebook account\Account 0")
+chrome_prefs = {
+    "profile.default_content_setting_values": {"images": 1}
+}
+options.add_experimental_option("prefs", chrome_prefs)
+options.add_argument(f"--user-data-dir=D:/Learn Python/Tool Python/Exercise/Chrome Selenium/Exercise Selenium/New User Facebook/Facebook account/Account 0")
 options.add_argument('--disable-notifications')
 # //*[@id="mount_0_0_6Y"]/div/div[1]/div/div[3]/div/div/div[1]/div[1]/div/div[3]/div/div /div[4]/div/div/div/div/div/div/div/div/div/div/div[2]/div[21]/div/div[2]/div/div[2]/ div/div/div[2]/div[1]/div/div/div[1]/span/span[1]/span/a
 # //*[@id="mount_0_0_6Y"]/div/div[1]/div/div[3]/div/div/div[1]/div[1]/div/div[3]/div/div/div[4]/div/div/div/div/div/div/div/div/div/div/div[2]/div[21]/div/div[2]/div/div[1]/div/div/div[2]/div[1]/div/div/div[1]/span/span[1]/span/a
 driver = Chrome(options=options)
-driver.get("https://www.facebook.com/groups/146473906768401/members")
-x_path = str(input("Input XPATH: "))
-x_path = list(x_path)
-x_new = ""
-for i in range(2,11):
+driver.maximize_window()
+driver.implicitly_wait(10)
+# driver.get("https://www.facebook.com/")
+# sleep(100)
+driver.get("https://www.facebook.com/messages/t/100064994050342")
+
+# f = driver.find_element(By.XPATH, "/html/body/div[1]/div/div[1]/div/div[3]/div/div/div[1]/div[1]/div/div/div[1]/div[2]/div/div/div/div[4]/div/div/div[1]/div/div/div/div[1]/div[2]/span/span")
+# f.click()
+f1 = driver.find_element(By.XPATH,"/html/body/div[1]/div/div[1]/div/div[3]/div/div/div[1]/div[1]/div[2]/div/div/div/div[1]/div/div/div/div/div/div[2]/div/div/div[2]/div/div/div[4]/div[2]/div/div[1]/div[1]/p")
+f1.send_keys("aaaa",Keys.ENTER)
+driver.find_element(By.XPATH,"/html/body/div[1]/div/div[1]/div/div[3]/div/div/div[1]/div[1]/div[2]/div/div/div/div[1]/div/div/div/div/div/div[2]/div/div/div[2]/div/div/div[1]/span/div").click()
+sleep(3)
+
+
+# Find the window with the title "Open"
+open_windows = gw.getWindowsWithTitle("Open")
+
+# Check if any windows are found
+if open_windows:
+    # If windows are found, activate the first one
+    open_window = open_windows[0]
     
-    for j in x_path:
-        x_new+=j
-    
-    f = driver.find_element(By.XPATH,x_new.replace('"',"'"))
-    text = str(f.get_attribute("href"))
-    reg = re.compile(r"user\/(.*?)\/")
-    ret = reg.findall(text)
-    print(ret[0])
-    x_new = ""
-    x_path[168] = str(i)
+    # Activate the window
+    open_window.activate()
 
+    # Wait briefly for the window to become active
+    sleep(0.5)
 
+    # Type the file path
+    file_path = os.getcwd() + "\\1.png"
+    pyautogui.write(file_path)
+    sleep(0.5)
+    # Press Enter
+    pyautogui.press("tab")
+    pyautogui.press("enter")
+    driver.find_element(By.XPATH,"/html/body/div[1]/div/div[1]/div/div[3]/div/div/div[1]/div[1]/div[2]/div/div/div/div[1]/div/div/div/div/div/div[2]/div/div/div[2]/div/span[2]/div").click()
+else:
+    print("Window with the title 'Open' not found.")
+# f2.send_keys("D:/Learn Python/Tool Python/Exercise/Chrome Selenium/Exercise Selenium/New User Facebook/1.png",Keys.ENTER)
 
-sleep(100)
+# for i in range(10):
+#     f = driver.find_element(By.XPATH,f"/html/body/div[1]/div/div[1]/div/div[3]/div/div/div[1]/div[1]/div/div[2]/div/div/div[4]/div/div/div/div/div/div/div/div/div/div/div[2]/div[21]/div/div[2]/div/div[{i+1}]/div/div/div[2]/div[1]/div/div/div[1]/span/span[1]/span/a")
+#     print(f.get_attribute('href'))
+# /html/body/div[1]/div/div[1]/div/div[3]/div/div/div[1]/div[1]/div/div[2]/div/div/div[4]/div/div/div/div/div/div/div/div/div/div/div[2]/div[21]/div/div[2]/div/div[1]/div/div/div[2]/div[1]/div/div/div[1]/span/span[1]/span/a
+# /html/body/div[1]/div/div[1]/div/div[3]/div/div/div[1]/div[1]/div/div[2]/div/div/div[4]/div/div/div/div/div/div/div/div/div/div/div[2]/div[21]/div/div[2]/div/div[1]/div/div/div[2]/div[1]/div/div/div[1]/span/span[1]/span/a
+# /html/body/div[1]/div/div[1]/div/div[3]/div/div/div[1]/div[1]/div/div[2]/div/div/div[4]/div/div/div/div/div/div/div/div/div/div/div[2]/div[21]/div/div[2]/div/div[2]/div/div/div[2]/div[1]/div/div/div[1]/span/span[1]/span/a
+sleep(1000)
 driver.close()
