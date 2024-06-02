4f06f36d3ab35fb2976a6663e323e81e4014310a
PHAMTIEN04 <phamphuoctien177@gmail.com>
2024-06-02 21:30:20 +0700

    update

--- Tool Python/Exercise/Chrome Selenium/Exercise Selenium/New User Facebook/main.py
+++ Tool Python/Exercise/Chrome Selenium/Exercise Selenium/New User Facebook/main.py
@@ -14,19 +14,20 @@ def set_up():
     chrome_prefs = {
         "profile.default_content_setting_values": {"images": 1}
     }
+    #1die
     options.add_experimental_option("prefs", chrome_prefs)
-    options.add_argument(f"--user-data-dir=D:/Learn Python/Tool Python/Exercise/Chrome Selenium/Exercise Selenium/New User Facebook/Facebook account/Account 0")
+    options.add_argument(f"--user-data-dir=D:/Learn Python/Tool Python/Exercise/Chrome Selenium/Exercise Selenium/New User Facebook/Facebook Account/Account 2")
     options.add_argument('--disable-notifications')
     driver = Chrome(options=options)
     driver.maximize_window()
-    driver.implicitly_wait(10)
+    driver.implicitly_wait(30)
     return driver
 # Login
 def login(driver):
     try:
         driver.get("https://www.facebook.com/")
-        sleep(0.5)
-        path = os.getcwd() + "\\New User Facebook\\account.txt"
+        sleep(111110.5)
+        path = "D:/Learn Python/Tool Python/Exercise/Chrome Selenium/Exercise Selenium/New User Facebook/account.txt"
         with open(path,"r") as ac_r:
             read = ac_r.read()
             ac_r.close()
@@ -42,19 +43,19 @@ def login(driver):
             print("Login Sussccess!!!")
 
         sleep(3)
-    except:
+    except Exception as e:
         print("Login Error!!!")
+        print(e)
         exit()
 
 def Get_ID_New_User(driver):
 # Get ID New User
     try:
         driver.get("https://www.facebook.com/groups/146473906768401/members")
-        x = 1
+        x = 17
         while True:
             try:
-                for i in range(10):
-                    f = driver.find_element(By.XPATH,f"/html/body/div[1]/div/div[1]/div/div[3]/div/div/div[1]/div[1]/div/div[2]/div/div/div[4]/div/div/div/div/div/div/div/div/div/div/div[2]/div[{x}]/div/div[2]/div/div[{i+1}]/div/div/div[2]/div[1]/div/div/div[1]/span/span[1]/span/a")
+                f = driver.find_element(By.XPATH,f"/html/body/div[1]/div/div[1]/div/div[3]/div/div/div[1]/div[1]/div/div[2]/div/div/div[4]/div/div/div/div/div/div/div/div/div/div/div[2]/div[{x}]/div/div[2]/div/div[1]/div/div/div[2]/div[1]/div/div/div[1]/span/span[1]/span/a")
                 break
             except:
                 print("Bruceforce x:",x)
@@ -67,14 +68,15 @@ def Get_ID_New_User(driver):
             list_id.append(crg.group(1))
         print("Get ID Sussccess!!!")
         return list_id
-    except:
+    except Exception as e:
         print("Get ID Error!!!")
+        print(e)
         exit()
 # Check User trung lap
 def check_user(list_id):
     try:
         list_id_old = []
-        path = os.getcwd() + "\\New User Facebook\\id.txt"
+        path = "D:/Learn Python/Tool Python/Exercise/Chrome Selenium/Exercise Selenium/New User Facebook/id.txt"
         with open(path,"r") as ac_r:
             a = ac_r.read()
             ac_r.close()
@@ -92,8 +94,9 @@ def check_user(list_id):
 
         list_id_old = a.split("\n")
         return list_id_old
-    except:
+    except Exception as e:
         print("Check User Error!!!")
+        print(e)
         exit()
 # print(list_id)
 # # sleep(100)
@@ -102,10 +105,10 @@ def Add_Friend(driver,id):
         driver.get(f"https://www.facebook.com{id}")
         sleep(2)
         f = driver.find_element(By.CLASS_NAME,"x1lliihq.x6ikm8r.x10wlt62.x1n2onr6.xlyipyv.xuxw1ft")
-        if(f.text == "Add friend"):
+        if(f.text == "Add friend") or (f.text == "Thêm bạn bè"):
             f.click()
             print("Add Friend thanh cong!!!")
-                
+        sleep(2)
     except:
         print("Khong co nut Add Friend!!!")
 def send_messages(driver,list_id_old):
@@ -115,7 +118,7 @@ def send_messages(driver,list_id_old):
                 Add_Friend(driver,i)
                 try:
                     driver.get(f"https://www.facebook.com/messages/t{i}")
-                    sleep(0.5)
+                    sleep(3)
 
                     t1 = random.choice(["Chào bạn!","Hi bạn","Chào bạn","Hello bạn"])
                     driver.find_element(By.XPATH,"//p[contains(@class, 'xat24cr xdj266r')]").send_keys(t1,Keys.ENTER)
@@ -124,34 +127,9 @@ def send_messages(driver,list_id_old):
                     sleep(3)
                     driver.find_element(By.XPATH,"//p[contains(@class, 'xat24cr xdj266r')]").send_keys("Mình là Đối Tác Onus có nhiệm vụ hỗ trợ và hướng dẫn người mới vào nhóm",Keys.ENTER)
                     sleep(3)
-                    driver.find_element(By.XPATH,"//p[contains(@class, 'xat24cr xdj266r')]").send_keys("Bạn làm theo các bước dưới đây để mình có thể dễ dàng hỗ trợ bạn hơn nha",Keys.ENTER)
-                    sleep(3)
-                    driver.find_element(By.XPATH,"/html/body/div[1]/div/div[1]/div/div[3]/div/div/div[1]/div[1]/div[2]/div/div/div/div[1]/div/div/div/div/div/div[2]/div/div/div[2]/div/div/div[1]/span/div").click()
-                    sleep(3)
-                    open_windows = gw.getWindowsWithTitle("Open")
-
-                    if open_windows:
-
-                        open_window = open_windows[0]
-                        open_window.activate()
-
-                        sleep(0.5)
-
-                        file_path = os.getcwd() + "\\New User Facebook\\1.jpg"
-
-                        pyautogui.write(file_path)
-                        sleep(0.5)
-
-
-                        pyautogui.press("enter")
-                        sleep(3)
-                        driver.find_element(By.XPATH,"/html/body/div[1]/div/div[1]/div/div[3]/div/div/div[1]/div[1]/div[2]/div/div/div/div[1]/div/div/div/div/div/div[2]/div/div/div[2]/div/span[2]/div").click()
-                    else:
-                        print("Window with the title 'Open' not found.")
-                    sleep(0.5)
-                    driver.find_element(By.XPATH,"//p[contains(@class, 'xat24cr xdj266r')]").send_keys("À với bên mình đang có chương trình hỗ trợ người mới khi hoàn thành các bước ở trên sẽ được nhận free Voucher Futures 200k",Keys.ENTER)
-                    sleep(3)
-                    driver.find_element(By.XPATH,"/html/body/div[1]/div/div[1]/div/div[3]/div/div/div[1]/div[1]/div[2]/div/div/div/div[1]/div/div/div/div/div/div[2]/div/div/div[2]/div/div/div[1]/span/div").click()
+                    driver.find_element(By.XPATH,"//p[contains(@class, 'xat24cr xdj266r')]").send_keys("Bạn đã nhận voucher 300k trải nghiệm Onus chưa ạ! Nếu chưa bạn làm theo các bước dưới đây nha",Keys.ENTER)
+                    sleep(3)                       
+                    driver.find_element(By.XPATH,"/html/body/div[1]/div/div[1]/div/div[3]/div/div/div[1]/div[1]/div[2]/div/div/div/div[1]/div/div/div/div[1]/div/div[2]/div/div/div/div[2]/div/div/div[1]").click()
                     sleep(3)
                     open_windows = gw.getWindowsWithTitle("Open")
 
@@ -160,21 +138,45 @@ def send_messages(driver,list_id_old):
                         open_window = open_windows[0]
                         open_window.activate()
 
-                        sleep(0.5)
+                        sleep(3)
 
-                        file_path = os.getcwd() + "\\New User Facebook\\voucher.jpg"
+                        file_path = r"D:\Learn Python\Tool Python\Exercise\Chrome Selenium\Exercise Selenium\New User Facebook\voucher.jpg"
+
                         pyautogui.write(file_path)
                         sleep(0.5)
 
 
                         pyautogui.press("enter")
-                        sleep(3)
-                        driver.find_element(By.XPATH,"/html/body/div[1]/div/div[1]/div/div[3]/div/div/div[1]/div[1]/div[2]/div/div/div/div[1]/div/div/div/div/div/div[2]/div/div/div[2]/div/span[2]/div").click()
+                        sleep(0.5)
+                        driver.find_element(By.XPATH,"/html/body/div[1]/div/div[1]/div/div[3]/div/div/div[1]/div[1]/div[2]/div/div/div/div/div/div/div/div[1]/div/div[2]/div/div/div/div[2]/div/span[2]/div").click()
                     else:
                         print("Window with the title 'Open' not found.")
                     sleep(3)
                     driver.find_element(By.XPATH,"//p[contains(@class, 'xat24cr xdj266r')]").send_keys("Bạn làm xong gửi mình ID Onus để mình tặng voucher và mời vào nhóm Tín Hiệu Onus nha",Keys.ENTER)
-                    sleep(5)
+                    sleep(3)
+                    driver.find_element(By.XPATH,"/html/body/div[1]/div/div[1]/div/div[3]/div/div/div[1]/div[1]/div[2]/div/div/div/div[1]/div/div/div/div[1]/div/div[2]/div/div/div/div[2]/div/div/div[1]").click()
+                    sleep(3)
+                    open_windows = gw.getWindowsWithTitle("Open")
+
+                    if open_windows:
+
+                        open_window = open_windows[0]
+                        open_window.activate()
+
+                        sleep(3)
+
+                        file_path = r"D:\Learn Python\Tool Python\Exercise\Chrome Selenium\Exercise Selenium\New User Facebook\voucher1.png"
+
+                        pyautogui.write(file_path)
+                        sleep(0.5)
+
+
+                        pyautogui.press("enter")
+                        sleep(0.5)
+                        driver.find_element(By.XPATH,"/html/body/div[1]/div/div[1]/div/div[3]/div/div/div[1]/div[1]/div[2]/div/div/div/div/div/div/div/div[1]/div/div[2]/div/div/div/div[2]/div/span[2]/div").click()
+                    else:
+                        print("Window with the title 'Open' not found.")
+                    sleep(3)
                     print(f"ID:{i} Sussccess")
                 except Exception as e:
                     print(f"ID:{i} Error!!")
@@ -184,8 +186,9 @@ def send_messages(driver,list_id_old):
             
     except:
         print("Messages Error!!!")
-
-    sleep(3600)
+        
+    print("Wait 1 hour for it to run automatically again!!!")
+    sleep(10)
     driver.close()
 
 def main():
@@ -195,6 +198,7 @@ def main():
         get_id = Get_ID_New_User(driver)
         check = check_user(get_id)
         send_messages(driver,check)
+        sleep(3600)
 
 if __name__ == "__main__":
     main()
